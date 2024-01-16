ca65fc64e609fe8f26096b63d480b33abb2dc89d
Tomáš Mach <tomas15420@gmail.com>
2024-01-16 15:26:41 +0100

    designed login and register pages

--- src/main/java/cz/uhk/fim/servicebookapp/dto/UserRegisterDto.java
+++ src/main/java/cz/uhk/fim/servicebookapp/dto/UserRegisterDto.java
@@ -9,8 +9,11 @@ public class UserRegisterDto {
     @Size(min = 3, max = 24, message = "Uživatelské jméno může mít minimálně 3 a maximálně 24 znaků")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Uživatelské jméno může obsahovat pouze písmena a číslice")
     private String username;
-    @Email(message = "Zadejte platný email")
+    @NotBlank(message = "E-Mail musí být vyplněn")
+    @Email(message = "E-Mail musí být platný")
     private String email;
+    @NotBlank(message = "Heslo musí být vyplněno")
+    @Size(min = 8, max = 32, message = "Heslo musí mít minimálně 8 znaků a maximálně 32 znaků")
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[/*\\-+#%$=´()§,.~ˇ^˘°˛`˙´˝¨@&}{<>Đ\\[\\]Ł€]).*$", message = "Heslo musí obsahovat malé a velké písmeno, číslo a speciální znak")
     private String password;
     @NotBlank(message = "Prosím potvrďte heslo")
