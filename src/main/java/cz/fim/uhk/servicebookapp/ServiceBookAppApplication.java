e7473cec33ea152ecccb976c893a14a5730018b4
Tomáš Mach <tomas15420@gmail.com>
2024-01-16 08:13:35 +0100

    Initial commit

---
+++ src/main/java/cz/fim/uhk/servicebookapp/ServiceBookAppApplication.java
@@ -0,0 +1,13 @@
+package cz.fim.uhk.servicebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/fim/uhk/servicebookapp/ServiceBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fim.uhk.servicebookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
